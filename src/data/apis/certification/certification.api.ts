f7e4e67ef2d8535dca75e7fdfe14b5fe9cea9a4e
schatzen <gomieti@naver.com>
2022-09-13 19:23:43 +0900

    fix: 리다이렉트 경로 제거 및 팝업 설정 변경

--- src/data/apis/certification/certification.api.ts
+++ src/data/apis/certification/certification.api.ts
@@ -15,10 +15,10 @@ class IamCertificationApiService implements IamCertificationApiInterface {
     return window.IMP.certification(
       {
         // 모바일환경에서 popup:false(기본값) 인 경우 필수, 예: https://www.myservice.com/payments/complete/mobile
-        m_redirect_url: "https://www.naver.com",
+        // m_redirect_url: "https://www.naver.com",
 
         // PC환경에서는 popup 파라메터가 무시되고 항상 true 로 적용됨
-        popup: false,
+        popup: true,
       },
       isSuccess,
       isError
