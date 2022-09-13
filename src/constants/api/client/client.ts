d47902687454e9bf41ae9fecb7d09b989297a607
schatzen <gomieti@naver.com>
2022-09-13 19:06:58 +0900

    fix: 기본 도메인에 포트제거

--- src/constants/api/client/client.ts
+++ src/constants/api/client/client.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosClient = axios.create({
-  baseURL: "http://ssakduk-api.ureca.im:3000/api",
+  baseURL: "http://ssakduk-api.ureca.im/api",
   //baseURL: "http://192.168.0.32:3001/api",
 });
