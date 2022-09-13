86bad19ebfe76bcd4973635bcda633471635d523
schatzen <gomieti@naver.com>
2022-09-13 19:13:26 +0900

    fix: 도메인 http에서 https 로 변경

--- src/constants/api/client/client.ts
+++ src/constants/api/client/client.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosClient = axios.create({
-  baseURL: "http://ssakduk-api.ureca.im/api",
+  baseURL: "https://ssakduk-api.ureca.im/api",
   //baseURL: "http://192.168.0.32:3001/api",
 });
