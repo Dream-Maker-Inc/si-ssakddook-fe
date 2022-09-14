134389d448a95d0a4f9a7a58027b163eb853243e
schatzen <gomieti@naver.com>
2022-09-14 18:41:37 +0900

    fix : setting config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-};
-
-module.exports = {
   images: {
     loader: "akamai",
-    path: "/",
+    path: "./",
   },
+  assetPrefix: ".",
 };
+
+module.exports = nextConfig;
