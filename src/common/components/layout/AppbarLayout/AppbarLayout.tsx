1e42ff28defe28bbef29283ba2edfe27567678af
schatzen <gomieti@naver.com>
2022-09-15 19:43:54 +0900

    fix : 댓글쓰기 페이지 일 경우 패딩 변경

--- src/common/components/layout/AppbarLayout/AppbarLayout.tsx
+++ src/common/components/layout/AppbarLayout/AppbarLayout.tsx
@@ -2,16 +2,20 @@ import { css } from "@emotion/react";
 
 type LayoutProps = {
   children: React.ReactNode;
+  hasCommentWriteSection?: boolean;
 };
 
-export const AppbarLayout = ({ children }: LayoutProps) => {
-  return <div css={sx.container}>{children}</div>;
+export const AppbarLayout = ({
+  children,
+  hasCommentWriteSection = false,
+}: LayoutProps) => {
+  return <div css={sx.container(hasCommentWriteSection)}>{children}</div>;
 };
 
 const sx = {
-  container: css`
+  container: (hasCommentWriteSection: boolean) => css`
     width: 100%;
     height: 100%;
-    padding: 50px 0;
+    padding: ${hasCommentWriteSection ? "50px 0 40px 0" : "50px 0"};
   `,
 };
