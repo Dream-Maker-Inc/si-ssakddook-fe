ab17fdd303da504b56cf93ae50b2bd09e8ce458d
schatzen <gomieti@naver.com>
2022-09-15 15:23:06 +0900

    fix: 운영 서버 url 로 변경

--- src/constants/api/client/client.ts
+++ src/constants/api/client/client.ts
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export const axiosClient = axios.create({
   baseURL: "https://ssakduk-api.ureca.im/api",
-  //baseURL: "http://192.168.0.32:3001/api",
+  // baseURL: "http://192.168.0.32:3001/api",
 });
