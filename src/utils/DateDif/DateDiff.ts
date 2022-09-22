8bfc0bde632fb3671e5cdc9f53424ef0a0d395e0
schatzen <gomieti@naver.com>
2022-09-22 16:53:44 +0900

    feat: 날짜 format 기능 추가

---
+++ src/utils/DateDif/DateDiff.ts
@@ -0,0 +1,10 @@
+import { differenceInDays } from "date-fns";
+
+export const getDateDiff = (date: Date) => {
+  const firstDate = date == undefined ? new Date() : date;
+  const secondDate = new Date();
+
+  const dateDiff = differenceInDays(secondDate, new Date(firstDate));
+
+  return dateDiff == 0 ? "오늘" : dateDiff == 1 ? "어제" : `${dateDiff}일 전`;
+};
