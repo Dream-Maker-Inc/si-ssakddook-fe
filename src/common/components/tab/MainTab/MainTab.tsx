dfbfa4102206c9c3db16b970d2619d8eb0071f28
schatzen <gomieti@naver.com>
2022-09-22 16:57:38 +0900

    fix: padding 수정 및 div 정리

--- src/common/components/tab/MainTab/MainTab.tsx
+++ src/common/components/tab/MainTab/MainTab.tsx
@@ -9,54 +9,34 @@ type MainTabProps = {
 };
 export const MainTab = ({ username }: MainTabProps) => {
   return (
-    <div>
-      <div css={sx.container}>
-        <div css={sx.appbarContainer}>
-          <IconButton>
-            <Image
-              width="24px"
-              height="16px"
-              src="/img/main/logo.svg"
-              alt="logo"
-            />
-          </IconButton>
-          <Typography variant="h2" ml="12px" sx={{ flexGrow: 1 }}>
-            {username}님, 환영해요.
-          </Typography>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-          >
-            <NotificationsOutlinedIcon
-              color="secondary"
-              sx={{ color: LightColor.TextMain }}
-            />
-          </IconButton>
-        </div>
-      </div>
+    <div css={sx.container}>
+      <IconButton>
+        <Image width="24px" height="16px" src="/img/main/logo.svg" alt="logo" />
+      </IconButton>
+      <Typography variant="h2" ml="12px" sx={{ flexGrow: 1 }}>
+        {username}님, 환영해요.
+      </Typography>
+      <IconButton size="large" edge="start" color="inherit" aria-label="menu">
+        <NotificationsOutlinedIcon
+          color="secondary"
+          sx={{ color: LightColor.TextMain }}
+        />
+      </IconButton>
     </div>
   );
 };
 
 const sx = {
   container: css`
+    position: absolute;
     width: 100%;
     height: 50px;
+    left: 0px;
+    top: 0px;
 
-    position: fixed;
-    top: 0;
-    left: 0;
-  `,
+    padding: 0 16px;
 
-  appbarContainer: css`
-    width: 100%;
-    height: 100%;
-    padding: 0 18px;
-    background-color: white;
-    box-shadow: none;
-    border-bottom: 2px solid ${LightColor.Gray500};
+    box-shadow: 0px 1px 1px rgba(0, 0, 0, 0.1);
 
     display: flex;
     align-items: center;
