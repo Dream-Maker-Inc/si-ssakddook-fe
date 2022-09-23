90c5aefdca2c96343ef1f03d6ee8771673959abf
schatzen <gomieti@naver.com>
2022-09-23 14:20:51 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
+    domains: ["cdn.pixabay.com", "ureca.s3.ap-northeast-2.amazone.com"],
     loader: "akamai",
     path: "/",
   },
