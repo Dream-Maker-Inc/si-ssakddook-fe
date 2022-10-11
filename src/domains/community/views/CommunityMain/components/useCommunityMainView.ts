9f6f68b183e751c0a0e3551439bfce9829cd7eb3
schatzen <56854068+schatzen@users.noreply.github.com>
2022-10-11 19:46:36 +0900

    fix: category 아이콘 컴포넌트 추가

--- src/domains/community/views/CommunityMain/components/useCommunityMainView.ts
+++ src/domains/community/views/CommunityMain/components/useCommunityMainView.ts
@@ -1,73 +1,84 @@
 import { RoutePath } from "@/constants/Path";
 import { useFindAllPost } from "@/data/apis/posting/usePostingApiHooks";
 import { useRouter } from "next/router";
+import IconPerson from "@/img/community/boxIcons/icon-person.svg";
+import IconHeart from "@/img/community/boxIcons/icon-heart.svg";
+import IconSchool from "@/img/community/boxIcons/icon-school.svg";
+import IconHouse from "@/img/community/boxIcons/icon-house.svg";
+import IconViolence from "@/img/community/boxIcons/icon-violence.svg";
+import IconCyberViolence from "@/img/community/boxIcons/icon-cyber-violence.svg";
+import IconEmoji from "@/img/community/boxIcons/icon-emoji.svg";
+import IconGift from "@/img/community/boxIcons/icon-gift.svg";
+import IconStar from "@/img/community/boxIcons/icon-star.svg";
+import IconConcern from "@/img/community/boxIcons/icon-concern.svg";
+import IconEtc from "@/img/community/boxIcons/icon-etc.svg";
 
 export const useCommunityMainView = () => {
   const router = useRouter();
   const communitBoxInfos = [
     {
-      imgSrc: "/img/community/boxIcons/icon-person.svg",
-      hoverImgSrc: "/img/community/hoverBoxIcons/icon-person.svg",
+      imgSrc: IconPerson,
+      hoverImgSrc: IconPerson,
       address: "/community",
       content: "직장 폭력",
     },
     {
-      imgSrc: "/img/community/boxIcons/icon-heart.svg",
-      hoverImgSrc: "/img/community/hoverBoxIcons/icon-person.svg",
+      imgSrc: IconHeart,
+      hoverImgSrc: IconHeart,
       address: "/community",
       content: "데이트 폭력",
     },
     {
-      imgSrc: "/img/community/boxIcons/icon-school.svg",
-      hoverImgSrc: "/img/community/hoverBoxIcons/icon-person.svg",
+      imgSrc: IconSchool,
+      hoverImgSrc: IconSchool,
       address: "/community",
       content: "학교 폭력",
     },
     {
-      imgSrc: "/img/community/boxIcons/icon-house.svg",
-      hoverImgSrc: "/img/community/hoverBoxIcons/icon-person.svg",
+      imgSrc: IconHouse,
+      hoverImgSrc: IconHouse,
       address: "/community",
       content: "가정 폭력",
     },
     {
-      imgSrc: "/img/community/boxIcons/icon-violence.svg",
-      hoverImgSrc: "/img/community/hoverBoxIcons/icon-person.svg",
+      imgSrc: IconViolence,
+      hoverImgSrc: IconViolence,
       address: "/community",
       content: "성폭력",
     },
     {
-      imgSrc: "/img/community/boxIcons/icon-cyber-violence.svg",
-      hoverImgSrc: "/img/community/hoverBoxIcons/icon-person.svg",
+      imgSrc: IconCyberViolence,
+      hoverImgSrc: IconCyberViolence,
       address: "/community",
       content: "사이버 폭력",
     },
     {
-      imgSrc: "/img/community/boxIcons/icon-emoji.svg",
-      hoverImgSrc: "/img/community/hoverBoxIcons/icon-person.svg",
+      imgSrc: IconEmoji,
+      hoverImgSrc: IconEmoji,
       address: "/community",
       content: "우울증",
     },
     {
-      imgSrc: "/img/community/boxIcons/icon-gift.svg",
-      hoverImgSrc: "/img/community/hoverBoxIcons/icon-person.svg",
+      imgSrc: IconGift,
+      hoverImgSrc: IconGift,
       address: "/community",
       content: "비밀",
     },
     {
-      imgSrc: "/img/community/boxIcons/icon-star.svg",
-      hoverImgSrc: "/img/community/hoverBoxIcons/icon-person.svg",
+      imgSrc: IconStar,
+      hoverImgSrc: IconStar,
       address: "/community",
       content: "취미 공유",
     },
     {
-      imgSrc: "/img/community/boxIcons/icon-concern.svg",
-      hoverImgSrc: "/img/community/hoverBoxIcons/icon-person.svg",
+      imgSrc: IconConcern,
+      hoverImgSrc: IconConcern,
       address: "/community",
       content: "고민",
     },
     {
-      imgSrc: "/img/community/boxIcons/icon-etc.svg",
-      hoverImgSrc: "/img/community/hoverBoxIcons/icon-person.svg",
+      imgSrc: IconEtc,
+      hoverImgSrc: IconEtc,
       address: "/community",
       content: "기타",
     },
