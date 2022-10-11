da32577f666ed5bb4ce6a0cba998883cd69c5a6a
schatzen <56854068+schatzen@users.noreply.github.com>
2022-10-11 19:54:45 +0900

    update: 더보기 아이콘 컴포넌트 수정

--- src/domains/more/More/useMoreView.ts
+++ src/domains/more/More/useMoreView.ts
@@ -1,26 +1,29 @@
 import { RoutePath } from "@/constants/Path";
 import { ClickBoxProps } from "../types/MoreType.type";
+import IconSmile from "@/img/more/icon-smile.svg";
+import IconSetting from "@/img/more/icon-setting.svg";
+import IconMore from "@/img/more/icon-more.svg";
 
 export const useMoreView = () => {
   const models: ClickBoxProps[] = [
     {
       title: "내 정보",
       desc: "닉네임과 비밀번호를 변경할 수 있어요.",
-      iconSrc: "/img/more/icon-smile.svg",
+      iconSrc: IconSmile,
       nextButtonState: true,
       onClickPath: RoutePath.MyInformation,
     },
     {
       title: "설정",
       desc: "앱 설정을 변경할 수 있어요.",
-      iconSrc: "/img/more/icon-setting.svg",
+      iconSrc: IconSetting,
       nextButtonState: true,
       onClickPath: RoutePath.Setting,
     },
     {
       title: "기타",
       desc: "공지사항, 앱 정보, 회사 소개 등을 조회할 수 있어요.",
-      iconSrc: "/img/more/icon-more.svg",
+      iconSrc: IconMore,
       nextButtonState: true,
       onClickPath: RoutePath.Etc,
     },
--- src/domains/more/information/MyInformation/useMyInformationView.ts
+++ src/domains/more/information/MyInformation/useMyInformationView.ts
@@ -2,6 +2,11 @@ import { RoutePath } from "@/constants/Path";
 import { useGetCurrentMember } from "@/data/apis/member/useMemberApiHooks";
 import { useQuery } from "react-query";
 import { ClickBoxProps } from "../../types/MoreType.type";
+import IconId from "@/img/more/icon-id.svg";
+import IconSmile from "@/img/more/icon-smile.svg";
+import IconPassword from "@/img/more/icon-password.svg";
+import IconUnsmile from "@/img/more/icon-unsmile.svg";
+import IconExit from "@/img/more/icon-exit.svg";
 
 export const useMyInformationView = () => {
   const { data } = useQuery("get-curr-member", useGetCurrentMember);
@@ -10,42 +15,42 @@ export const useMyInformationView = () => {
     {
       title: "아이디",
       desc: data?.email as string,
-      iconSrc: "/img/more/icon-id.svg",
+      iconSrc: IconId,
       nextButtonState: false,
       onClickPath: RoutePath.MyInformation,
     },
     {
       title: "닉네임",
       desc: data?.nickname as string,
-      iconSrc: "/img/more/icon-smile.svg",
+      iconSrc: IconSmile,
       nextButtonState: true,
       onClickPath: RoutePath.ChangeNickname,
     },
     {
       title: "프로필 이미지",
       desc: "프로필 이미지를 변경할 수 있어요.",
-      iconSrc: "/img/more/icon-smile.svg",
+      iconSrc: IconSmile,
       nextButtonState: true,
       onClickPath: RoutePath.ChangeProfileImage,
     },
     {
       title: "비밀번호 변경",
       desc: "비밀번호를 잊으셨나요? 새로운 비밀번호로 변경할 수 있어요.",
-      iconSrc: "/img/more/icon-password.svg",
+      iconSrc: IconPassword,
       nextButtonState: true,
       onClickPath: RoutePath.ChangePassword,
     },
     {
       title: "로그아웃",
       desc: "로그아웃하면, 다음 접속 시 다시 로그인하셔야 해요.",
-      iconSrc: "/img/more/icon-unsmile.svg",
+      iconSrc: IconUnsmile,
       nextButtonState: true,
       onClickPath: RoutePath.Login,
     },
     {
       title: "회원 탈퇴",
       desc: "더이상 싹둑을 사용하지 않으시려면, 회원 탈퇴가 가능해요.",
-      iconSrc: "/img/more/icon-exit.svg",
+      iconSrc: IconExit,
       nextButtonState: true,
       onClickPath: RoutePath.Secession,
     },
