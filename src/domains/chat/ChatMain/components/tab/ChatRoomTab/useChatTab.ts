27e9aa8a83078676f5c7e50ee2ecb3c33e4ea2e8
schatzen <56854068+schatzen@users.noreply.github.com>
2022-10-11 17:33:26 +0900

    Update useChatTab.ts

--- src/domains/chat/ChatMain/components/tab/ChatRoomTab/useChatTab.ts
+++ src/domains/chat/ChatMain/components/tab/ChatRoomTab/useChatTab.ts
@@ -2,7 +2,7 @@ import LocalStorage from "@/data/LocalStorage/LocalStorage";
 import { ChatAtom } from "@/recoil/Navigation/Navigation.atom";
 import { useState } from "react";
 import { useSetRecoilState } from "recoil";
-import { useChannelActionContext, useChatContext } from "stream-chat-react";
+import { useChatContext } from "stream-chat-react";
 
 export const useChatTab = () => {
   const { channel, client } = useChatContext();
