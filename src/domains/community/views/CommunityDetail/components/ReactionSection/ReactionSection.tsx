66689a2d1287f326ca6bb024720cd53079212bff
NightOwl <steelman2019e@gmail.com>
2022-10-13 11:52:17 +0900

    fix: 커뮤니티 상세에서 좋아요 액션 시 터치 하이라이팅 애니메이션

--- src/domains/community/views/CommunityDetail/components/ReactionSection/ReactionSection.tsx
+++ src/domains/community/views/CommunityDetail/components/ReactionSection/ReactionSection.tsx
@@ -1,6 +1,6 @@
 import { LightColor } from "@/themes/Color";
 import { css } from "@emotion/react";
-import { IconButton, Typography, TypographyProps } from "@mui/material";
+import { Button, IconButton, Typography, TypographyProps } from "@mui/material";
 import Image from "next/image";
 import CommentIcon from "@/img/icon-chat-comment.svg";
 import ReportIcon from "@/img/icon-chat-report.svg";
@@ -14,7 +14,7 @@ type ReactionSectionProps = {
 
 export const ReactionSection = (props: ReactionSectionProps) => {
   return (
-    <div css={sx.reactionContainer}>
+    <div css={st.reactionContainer}>
       <LikeBox
         likeCount={props.likeState.likeCount!!}
         onLike={props.likeState.onLike}
@@ -26,7 +26,7 @@ export const ReactionSection = (props: ReactionSectionProps) => {
   );
 };
 
-const sx = {
+const st = {
   reactionContainer: css`
     width: 100%;
     height: 30px;
@@ -43,6 +43,15 @@ const sx = {
     justify-content: center;
     align-items: center;
   `,
+
+  button: css`
+    width: fit-content;
+    height: fit-content;
+    min-width: fit-content;
+    min-height: fit-content;
+    padding: 4px 10px;
+    border-radius: 4px;
+  `,
 };
 
 type LikeBoxProps = {
@@ -52,60 +61,60 @@ type LikeBoxProps = {
 };
 
 const LikeBox = ({ likeCount, onLike, isLike }: LikeBoxProps) => {
-  return (
-    <div css={sx.box}>
-      <IconButton onClick={onLike}>
-        {isLike ? (
-          <Image width="16px" height="16px" src={RedHeartIcon} alt="" />
-        ) : (
-          <Image width="16px" height="16px" src={HeartIcon} alt="" />
-        )}
+  const icon = isLike ? (
+    <Image width="16px" height="16px" src={RedHeartIcon} alt="" />
+  ) : (
+    <Image width="16px" height="16px" src={HeartIcon} alt="" />
+  );
 
-        <Typography
-          variant="body2"
-          lineHeight="1"
-          color={LightColor.TextMain}
-          ml="10px"
-        >
+  return (
+    <div css={st.box}>
+      <Button css={st.button} size={"small"} startIcon={icon} onClick={onLike}>
+        <Typography variant="body2" lineHeight="1" color={LightColor.TextMain}>
           {likeCount}
         </Typography>
-      </IconButton>
+      </Button>
     </div>
   );
 };
 
 const CommentBox = (p: TypographyProps) => {
   return (
-    <div css={sx.box}>
-      <IconButton>
-        <Image width="16px" height="16px" src={CommentIcon} alt="" />
+    <div css={st.box}>
+      <Button
+        css={st.button}
+        size={"small"}
+        startIcon={
+          <Image width="16px" height="16px" src={CommentIcon} alt="" />
+        }
+        disableRipple
+        disableFocusRipple
+        disableTouchRipple
+      >
         <Typography
           variant="body2"
           lineHeight="1"
           color={LightColor.TextMain}
-          ml="10px"
           {...p}
         />
-      </IconButton>
+      </Button>
     </div>
   );
 };
 
 const ReportBox = () => {
   return (
-    <div css={sx.box}>
-      <IconButton onClick={() => alert("준비중입니다.")}>
-        <Image width="16px" height="16px" src={ReportIcon} alt="" />
-
-        <Typography
-          variant="body2"
-          lineHeight="1"
-          color={LightColor.TextMain}
-          ml="10px"
-        >
+    <div css={st.box}>
+      <Button
+        css={st.button}
+        size={"small"}
+        startIcon={<Image width="16px" height="16px" src={ReportIcon} alt="" />}
+        onClick={() => alert("준비중입니다.")}
+      >
+        <Typography variant="body2" lineHeight="1" color={LightColor.TextMain}>
           신고하기
         </Typography>
-      </IconButton>
+      </Button>
     </div>
   );
 };
