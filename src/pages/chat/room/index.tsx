6d566844e2418d4c18a8bbdab1e173892f1ff7f1
schatzen <gomieti@naver.com>
2022-10-15 00:46:06 +0900

    Update index.tsx

--- src/pages/chat/room/index.tsx
+++ src/pages/chat/room/index.tsx
@@ -1,8 +1,10 @@
 import { ChatRoomView } from "@/domains/chat/ChatRoom";
+import { useUserSession } from "@/recoil/session/user-session.atom";
 import { NextPage } from "next";
 
 const ChatRoomPage: NextPage = () => {
-  return <ChatRoomView />;
+  const { user } = useUserSession();
+  return user && <ChatRoomView />;
 };
 
 export default ChatRoomPage;
