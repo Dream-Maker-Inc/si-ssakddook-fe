cdad0eed1954360100cd54b031bd37a0cbd02481
schatzen <56854068+schatzen@users.noreply.github.com>
2022-10-19 13:03:59 +0900

    update: 메인화면 상단 텍스트 닉네임으로 수정

--- src/domains/Main/useMainView.tsx
+++ src/domains/Main/useMainView.tsx
@@ -8,7 +8,7 @@ import { useQuery } from "react-query";
 export const useMainView = () => {
   const { data } = useQuery("get-curr-member", useGetCurrentMember);
 
-  const username = data?.name as string;
+  const nickname = data?.nickname as string;
 
   LocalStorage.setItem("id", data?.id + "");
   LocalStorage.setItem("nickname", data?.nickname + "");
@@ -33,6 +33,6 @@ export const useMainView = () => {
     "자책하지 말아요\n당신의 잘못이 아닙니다\n\n홀로 감당하기엔 너무 큰 고통\n앞으로는 함께 해결합시다\n\n누구에게도 말 못할 괴로움을\n여기 이곳에 두고 가시길";
   return {
     data: text,
-    username: username,
+    username: nickname,
   };
 };
