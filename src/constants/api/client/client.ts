db4771ba8b1b9a0858ff895280858ff1141da948
schatzen <gomieti@naver.com>
2022-12-08 16:15:41 +0900

    update: 비정상 접근시 error alert

--- src/constants/api/client/client.ts
+++ src/constants/api/client/client.ts
@@ -1,7 +1,8 @@
 import LocalStorage from "@/data/LocalStorage/LocalStorage";
 import { StatusCode } from "@/data/statusCode/StatusCode.enum";
 import { logoutAndGoSplash } from "@/domains/auth/functions/auth.func";
-import axios, { AxiosResponse } from "axios";
+import axios, { AxiosError, AxiosResponse } from "axios";
+import { useMemo } from "react";
 
 const axiosClient = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_CLIENT,
@@ -13,11 +14,15 @@ axiosClient.interceptors.request.use(function (config) {
   return config;
 });
 
-axiosClient.interceptors.response.use((res) => {
-  catchIfBlindMemberResponse(res);
-
-  return res;
-});
+axiosClient.interceptors.response.use(
+  (res) => {
+    catchIfBlindMemberResponse(res);
+    return res;
+  },
+  (err: AxiosError) => {
+    catchIfJwtExpiredResponse(err);
+  }
+);
 
 const axiosBasicClient = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_CLIENT,
@@ -28,6 +33,15 @@ const catchIfBlindMemberResponse = (res: AxiosResponse<any, any>) => {
   if (res?.data?.statusCode === StatusCode.BLIND_MEMBER) {
     alert(`(이용 불가) 정지된 계정의 활동 시도 입니다.`);
     logoutAndGoSplash();
+  }
+};
+
+// jwt 토큰이 만료 / 회원 탈퇴했을 떄 처리
+const catchIfJwtExpiredResponse = (res: AxiosError) => {
+  const status = res?.response?.status ?? 0;
+  if (status === 401) {
+    logoutAndGoSplash();
+    alert(`토큰이 만료 되었습니다. 다시 로그인 해주세요.`);
   }
 };
 
