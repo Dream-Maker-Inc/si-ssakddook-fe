98289a0e28b11130fbf60f3b01ccd5d2abc5f2fb
schatzen <gomieti@naver.com>
2022-12-12 20:28:49 +0900

    update: 이미지 교체

--- src/domains/auth/views/find/id/FindIdFail/FindIdFailView.tsx
+++ src/domains/auth/views/find/id/FindIdFail/FindIdFailView.tsx
@@ -2,27 +2,58 @@ import { Title } from "@/common/components/title/Title";
 import { css } from "@emotion/react";
 import { Button, Typography } from "@mui/material";
 import Image from "next/image";
-import IdFailImg from "@/img/find/id-fail.svg";
+import IdFailImg from "@/img/auth/img-id-search-failed.svg";
+import { useRouter } from "next/router";
+import { RoutePath } from "@/constants/Path";
 
 export const FindIdFailView = () => {
+  const router = useRouter();
+  const onLoginView = () => {
+    router.push(RoutePath.Login);
+  };
+
+  const onFindAccountView = () => {
+    router.push(RoutePath.FindAccount);
+  };
   return (
     <div css={sx.root}>
       <div css={sx.container}>
-        <Title>아이디 검색 실패</Title>
-        <Image width="250px" height="160px" src={IdFailImg} alt="img" />
-        <Typography variant="h4" textAlign={"center"} mt="60px">
+        <Typography
+          variant="h1"
+          mt="26px"
+          mb="12px"
+          width="100%"
+          textAlign="left"
+          fontSize="20px"
+        >
+          아이디 검색 실패
+        </Typography>
+        <div css={sx.image}>
+          <Image layout="fill" src={IdFailImg} alt="img" />
+        </div>
+        <Typography variant="h4" textAlign={"center"}>
           입력하신 휴대폰 번호와 일치하는 아이디를 찾지 못했어요.
           <br />
           입력하신 <span css={sx.span}>휴대폰 번호를 확인</span>하시고 다시
           시도해 주세요.
         </Typography>
         <div css={sx.buttonWrapper}>
-          <Button fullWidth variant="contained" color="primary">
+          <Button
+            fullWidth
+            variant="contained"
+            color="primary"
+            onClick={onFindAccountView}
+          >
             <Typography variant="body1" color="white">
               회원정보 다시 찾기
             </Typography>
           </Button>
-          <Button fullWidth variant="contained" color="secondary">
+          <Button
+            fullWidth
+            variant="contained"
+            color="secondary"
+            onClick={onLoginView}
+          >
             <Typography variant="body1" color="#606060">
               로그인하러 가기
             </Typography>
@@ -45,22 +76,22 @@ const sx = {
     display: flex;
     flex-direction: column;
     align-items: center;
-
     position: relative;
   `,
-
+  image: css`
+    position: relative;
+    width: 100%;
+    aspect-ratio: 1/1.02;
+  `,
   buttonWrapper: css`
     width: 100%;
     display: flex;
     flex-direction: column;
     gap: 16px;
-
     font-size: 14px;
-
     position: absolute;
     bottom: 0px;
   `,
-
   span: css`
     color: #5a8835;
   `,
--- src/domains/auth/views/find/id/FindIdSuccess/FindIdSuccessView.tsx
+++ src/domains/auth/views/find/id/FindIdSuccess/FindIdSuccessView.tsx
@@ -2,7 +2,7 @@ import { Title } from "@/common/components/title/Title";
 import { css } from "@emotion/react";
 import { Button, Typography } from "@mui/material";
 import Image from "next/image";
-import IdSuccessImg from "@/img/find/id-success.svg";
+import IdSuccessImg from "@/img/auth/img-id-search-success.svg";
 import { useRouter } from "next/router";
 import { RoutePath } from "@/constants/Path";
 
@@ -16,9 +16,20 @@ export const FindIdSuccessView = () => {
   return (
     <div css={sx.root}>
       <div css={sx.container}>
-        <Title>아이디 찾기 완료</Title>
-        <Image width="250px" height="160px" src={IdSuccessImg} alt="img" />
-        <Typography variant="h4" textAlign={"center"} mt="20px">
+        <Typography
+          variant="h1"
+          mt="26px"
+          mb="12px"
+          width="100%"
+          textAlign="left"
+          fontSize="20px"
+        >
+          아이디 찾기 완료
+        </Typography>
+        <div css={sx.image}>
+          <Image layout="fill" src={IdSuccessImg} alt="img" />
+        </div>
+        <Typography variant="h4" textAlign={"center"}>
           입력하신 휴대폰 번호와 일치하는 아이디를 찾았어요.
         </Typography>
         <div css={sx.idBox}>
@@ -60,37 +71,33 @@ const sx = {
     display: flex;
     flex-direction: column;
     align-items: center;
-
     position: relative;
   `,
-
+  image: css`
+    position: relative;
+    width: 100%;
+    aspect-ratio: 1/1.02;
+  `,
   span: css`
     color: #5a8835;
   `,
-
   idBox: css`
     width: 100%;
     height: 60px;
-
     background: rgba(90, 136, 53, 0.1);
     border-radius: 8px;
-
     display: flex;
     justify-content: center;
     align-items: center;
-
     margin-top: 20px;
     margin-bottom: 20px;
   `,
-
   buttonWrapper: css`
     width: 100%;
     display: flex;
     flex-direction: column;
     gap: 16px;
-
     font-size: 14px;
-
     position: absolute;
     bottom: 0px;
   `,
