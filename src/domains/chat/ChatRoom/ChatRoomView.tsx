26facf2ffdc2c94137634831257826f7918ce4e2
schatzen <gomieti@naver.com>
2022-12-15 15:10:14 +0900

    update: profile image user avatar에 반영 및 이미지 없을시 싹둑 로고로 대체

--- src/domains/chat/ChatRoom/ChatRoomView.tsx
+++ src/domains/chat/ChatRoom/ChatRoomView.tsx
@@ -1,14 +1,33 @@
 import { PlainLayout } from "@/common/components/layout/PlainLayout";
 import { css } from "@emotion/react";
-import { Channel, MessageInput, MessageList, Window } from "stream-chat-react";
+import {
+  Avatar,
+  Channel,
+  MessageInput,
+  MessageList,
+  Window,
+} from "stream-chat-react";
 import { ChatRoomTab } from "./components/tab/ChatRoomTab";
+import { useUserSession } from "@/recoil/session/user-session.atom";
 
 export const ChatRoomView = () => {
+  const { user } = useUserSession();
+
+  const UserChatProfileImage = () => {
+    const ssakdukLogoImageUrl = "https://ssakduk.com/global-assets/logo.png";
+
+    return (
+      <Avatar
+        image={user?.image == "" ? ssakdukLogoImageUrl : user?.image}
+        shape="circle"
+      />
+    );
+  };
   return (
     <PlainLayout isBottomMarginNecessary={false}>
       <ChatRoomTab />
       <div css={sx.root}>
-        <Channel>
+        <Channel Avatar={UserChatProfileImage}>
           <Window>
             <MessageList messageActions={[]} />
             <MessageInput />
