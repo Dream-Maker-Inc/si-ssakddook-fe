e3d69b5a1b3e2c70d83534594478dc8f93c0656f
schatzen <gomieti@naver.com>
2022-12-20 19:37:42 +0900

    update: 최신글 불러오는 api 함수 제거

--- src/domains/community/views/CommunityMain/components/useCommunityMainView.ts
+++ src/domains/community/views/CommunityMain/components/useCommunityMainView.ts
@@ -98,50 +98,7 @@ export const useCommunityMainView = () => {
     },
   ];
 
-  const size = 5;
-  const { data, isLoading, isError } = useFindAllPost(size);
-
-  const handleViewRecentPost = () => {
-    router.push({ pathname: RoutePath.CommunityList, query: { category: "" } });
-  };
-
-  if (!data) {
-    return {
-      fetchState: {
-        isLoading,
-        isError,
-      },
-      result: null,
-    };
-  }
-
-  const postings = mapToPostings(data.data.items);
-
   return {
     boxData: communitBoxInfos,
-    fetchState: {
-      isLoading,
-      isError,
-    },
-    result: postings,
-    onRecentView: handleViewRecentPost,
   };
-};
-
-const mapToPostings = (postingItemResponses: PostingItemResponse[]) => {
-  if (postingItemResponses === null || postingItemResponses === undefined) {
-    return null;
-  }
-
-  const postings = postingItemResponses.map((it) => ({
-    id: it.id,
-    title: _.truncate(it.title),
-    date: getTimeFromNow(it.createdAt),
-    nickname: it.author.nickname,
-    category: it.category,
-    likedCount: `${it.likedCount}`,
-    commentCount: `${it.commentCount}`,
-  }));
-
-  return postings;
 };
